d40653a6d89a4257e840d17174607e984ebc2431
Bryan Tempini <mistatempini@gmail.com>
2022-11-22 15:54:13 -0500

    finished questions flow and logic

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const questions = [
   "Please enter your employee ID.",
   "Please enter your email address.",
   "Please enter your office number.",
+  "What would you like to do?",
 ];
 
 //function to write README file
@@ -20,28 +21,93 @@ const writeHTMLFile = (html) =>
   );
 
 function init() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: questions[0],
-      name: "name",
-    },
-    {
-      type: "input",
-      message: questions[1],
-      name: "employeeID",
-    },
-    {
-      type: "input",
-      message: questions[2],
-      name: "emailAddress",
-    },
-    {
-      type: "input",
-      message: questions[3],
-      name: "officeNumber",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[0],
+        name: "managerName",
+      },
+      {
+        type: "input",
+        message: questions[1],
+        name: "managerID",
+      },
+      {
+        type: "input",
+        message: questions[2],
+        name: "emailAddress",
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "officeNumber",
+      },
+      {
+        type: "list",
+        message: questions[4],
+        choices: [
+          "Add an engineer.",
+          "Add an intern.",
+          "I am finished building my team.(Create HTML)",
+        ],
+        name: "toDo",
+      },
+    ])
+    .then((data) => {
+      if (data.toDo === "Add an engineer.") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Please enter engineers name.",
+            name: "employeeName",
+          },
+          {
+            type: "input",
+            message: "Please enter the employees ID number.",
+            name: "employeeID",
+          },
+          {
+            type: "input",
+            message: "Please enter the employees email address.",
+            name: "employeeEmail",
+          },
+          {
+            type: "input",
+            message: "Please enter the engineers github profile URL.",
+            name: "engineerGithub",
+          },
+        ]);
+      } else if (data.toDo === "Add an intern.") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Please enter interns name.",
+            name: "employeeName",
+          },
+          {
+            type: "input",
+            message: "Please enter the interns ID number.",
+            name: "employeeID",
+          },
+          {
+            type: "input",
+            message: "Please enter the interns email address.",
+            name: "employeeEmail",
+          },
+          {
+            type: "input",
+            message: "Please enter the interns school name.",
+            name: "internSchool",
+          },
+        ]);
+      }
+    });
 }
+
+// .then((data) => {
+//   const generatedResult = generateHTML(data);
+//   // writeHTMLFile(generatedResult);
+// });
 
 init();
